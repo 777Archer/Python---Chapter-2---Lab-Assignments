5370ab0aecfe1b8f8b4dd6f4a3858733c839dd96
777Archer <101377287+777Archer@users.noreply.github.com>
2022-05-01 15:06:11 -0700

    Update TestCase1.py

--- TestCase1.py
+++ TestCase1.py
@@ -1,11 +1,11 @@
 SALES_TAX = .07
 
 print("You'll be purchasing 5 items.")
-first_item_purchased = float(input ("Please enter the price of item 1: ")) 
-second_item_purchased = float(input ("Please enter the price of item 2: "))
-third_item_purchased = float(input ("Please enter the price of item 3: "))
-fourth_item_purchased = float(input ("Please enter the price of item 4: "))
-fifth_item_purchased = float(input ("Please enter the price of item 5: ") + '\n')
+first_item_purchased = float( input("Please enter the price of item 1: ")) 
+second_item_purchased = float( input("Please enter the price of item 2: "))
+third_item_purchased = float( input("Please enter the price of item 3: "))
+fourth_item_purchased = float( input("Please enter the price of item 4: "))
+fifth_item_purchased = float( input("Please enter the price of item 5: ") + '\n')
 
 subtotal = float(first_item_purchased + second_item_purchased + third_item_purchased + fourth_item_purchased + fifth_item_purchased)
 total_sales_tax = subtotal * SALES_TAX 
